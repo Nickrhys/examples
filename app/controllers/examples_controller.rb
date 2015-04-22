350ad3feac33be0f0b2115c04e3146e02f19844b
Nickrhys <nick@red56.uk>
2015-04-22 12:03:57 +0100

    can print params to the console via the controller

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -1,6 +1,11 @@
 class ExamplesController < ApplicationController
 
   def example1
-    
+   puts
+   puts "A text checkbox: #{(params[:a_checkbox_input])}"
+   puts "A hidden input: #{(params[:a_hidden_input])}"
+   puts "A text input: #{(params[:some_text])}"
+   puts "A submit input: #{params[:a_submit_input]}"
+   puts
   end
 end
